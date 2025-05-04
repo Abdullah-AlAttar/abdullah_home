ed94d04483d1ae7635c68b9c28039c97a3ab1849
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2025-05-04 11:18:18 +0200

    chore: add more programs

--- home.nix
+++ home.nix
@@ -5,6 +5,8 @@
   imports = [
     # Include the results of the hardware scan
     ./programs/zsh.nix
+    ./programs/zoxide.nix
+    ./programs/neofetch.nix
   ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
