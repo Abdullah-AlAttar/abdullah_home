d4948b300ebf7fa7aed3ab485ea764542233e832
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2025-05-16 19:56:37 +0200

    catpuccin

--- flake.nix
+++ flake.nix
@@ -8,18 +8,20 @@
     # Or use unstable: nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    catppuccin.url = "github:catppuccin/nix";
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager"; 
+      url = "github:nix-community/home-manager";
       # url = "github:nix-community/home-manager/archive/master.tar.gz";
-      
+
       # url = "github:nix-community/home-manager/archive/master.tar.gz";
       # Or use unstable: url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs"; # Make HM use the same nixpkgs
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, catppuccin, ... }@inputs:
     let
       # Replace <username> with your actual Linux username
       username = "ab_dullah";
@@ -38,6 +40,7 @@
           modules = [
             ./home.nix
             # You can add more modules here later if you split your config
+            catppuccin.homeModules.catppuccin
           ];
 
           # Optional: Extra arguments passed to your modules
