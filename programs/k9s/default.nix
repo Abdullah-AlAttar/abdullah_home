73f65ead3eab58d7dca953aaa19ad0de0330a2ab
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2025-05-24 17:29:59 +0200

    k9s

--- programs/k9s/default.nix
+++ programs/k9s/default.nix
@@ -21,25 +21,7 @@
     views = import ./views.nix;
     plugin = import ./plugins.nix;
 
-    skins = {
-      dracula = builtins.fromJSON (builtins.readFile ./skins/dracula.json);
-      catppuccin-mocha-transparent = builtins.fromJSON
-        (builtins.readFile ./skins/catppuccin-mocha-transparent.json);
-      catppuccin-frappe-transparent = builtins.fromJSON
-        (builtins.readFile ./skins/catppuccin-frappe-transparent.json);
-      catppuccin-latte-transparent = builtins.fromJSON
-        (builtins.readFile ./skins/catppuccin-latte-transparent.json);
-      catppuccin-macchiato-transparent = builtins.fromJSON
-        (builtins.readFile ./skins/catppuccin-macchiato-transparent.json);
-      catppuccin-macchiato =
-        builtins.fromJSON (builtins.readFile ./skins/catppuccin-macchiato.json);
-      catppuccin-frappe =
-        builtins.fromJSON (builtins.readFile ./skins/catppuccin-frappe.json);
-      catppuccin-latte =
-        builtins.fromJSON (builtins.readFile ./skins/catppuccin-latte.json);
-      catppuccin-mocha =
-        builtins.fromJSON (builtins.readFile ./skins/catppuccin-mocha.json);
-    };
+    skins =  import ./skins.nix;
   };
 }
 
