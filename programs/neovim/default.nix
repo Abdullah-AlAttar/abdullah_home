8de01b29141612a65acc4006a4f13ffec85a8418
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2025-07-23 14:51:26 +0200

    extra lua config

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
@@ -29,25 +29,6 @@
 
     luaLoader.enable = true;
 
-    extraConfigLua = ''
-      -- Highlight yanked text
-      vim.api.nvim_create_autocmd('TextYankPost', {
-        desc = 'Highlight when yanking (copying) text',
-        group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-        callback = function()
-          vim.highlight.on_yank({
-            higroup = 'IncSearch',  -- Highlight group to use
-            timeout = 300,          -- Duration in milliseconds
-          })
-        end,
-      })
-
-      -- Neovide-specific keymappings for scaling
-      if vim.g.neovide then
-          vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-          vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-          vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
-      end
-    '';
+    extraConfigLua = builtins.readFile ./extraConfigLua.lua;
   };
 }
