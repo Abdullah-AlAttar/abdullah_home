f51c81950ddc84f2630ddbbc871bdac5820ee6ac
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2025-07-23 11:42:39 +0200

    highlight on yank

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -30,6 +30,18 @@
     luaLoader.enable = true;
 
     extraConfigLua = ''
+      -- Highlight yanked text
+      vim.api.nvim_create_autocmd('TextYankPost', {
+        desc = 'Highlight when yanking (copying) text',
+        group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+        callback = function()
+          vim.highlight.on_yank({
+            higroup = 'IncSearch',  -- Highlight group to use
+            timeout = 300,          -- Duration in milliseconds
+          })
+        end,
+      })
+
       -- Neovide-specific keymappings for scaling
       if vim.g.neovide then
           vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
