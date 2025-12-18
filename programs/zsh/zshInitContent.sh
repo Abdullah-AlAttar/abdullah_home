295794fbe6cb9dd51d822b6c685612bb85768992
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2025-12-18 09:25:14 +0100

    add fcd

--- programs/zsh/zshInitContent.sh
+++ programs/zsh/zshInitContent.sh
@@ -1,5 +1,7 @@
-# [[ ! -f ${./config/p10k.zsh} ]] || source ${./config/p10k.zsh}
-# [[ ! -f "$HOME/.config/p10k.zsh" ]] || source "$HOME/.config/p10k.zsh"
 
-# https://github.com/atuinsh/atuin/issues/977#issuecomment-1548880080
-# zvm_after_init_commands+=(eval "$(atuin init zsh)")
+# Zsh function to change directory using fzf and fd
+fcd() {
+  local dir
+  dir="$(fd -t d | fzf)" || return
+  cd "$dir"
+}
